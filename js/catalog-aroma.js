23a502a29917e6d273585cb656f5e300fc228a5b
katya <katyamatya21@gmail.com>
2015-07-14 17:13:33 +0400

    666
    catalog aroma
    
    mobile

--- js/catalog-aroma.js
+++ js/catalog-aroma.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
 
     $('.name-filter').bind('click',function(e){
+        $(this).toggleClass("bigZ");
         $(this).next().toggle();
     });
 
     $('.hide-filter').bind('mouseleave',function(e){
         $(this).hide();
+        $(this).prev().removeClass("bigZ");
     });
 
 });
