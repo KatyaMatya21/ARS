70d85d0b7769ce38c8e3e9e1de68012a89d5669b
katya <katyamatya21@gmail.com>
2015-07-14 17:41:25 +0400

    666
    catalog aroma
    
    mobile slider

--- js/catalog-aroma.js
+++ js/catalog-aroma.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 
+    window.mobileMidSlider = null;
+
+    var gpuAcceleration = Modernizr.csstransforms3d ? 'translateZ(0) ' : '';
+    var transform = Modernizr.prefixed('transform');
+
+    var lazyResize = _.debounce( onViewportResize, 300 );
+    $(window).on('resize load', lazyResize);
+
     $('.name-filter').bind('click',function(e){
         $(this).toggleClass("bigZ");
         $(this).next().toggle();
@@ -10,4 +18,33 @@ $(document).ready(function(){
         $(this).prev().removeClass("bigZ");
     });
 
+    function onViewportResize() {
+        var win = $(this);
+
+        // media breakpoint for 480px
+        if( win.width() <= 480 ) {
+
+
+            // Handle mobile-only mid slider
+            window.mobileMidSlider = new Sly($('#mobile-mid-slider'), {
+                mouseDragging: 1,
+                touchDragging: 1,
+                horizontal: 1,
+                itemNav: 'forceCentered',
+                smart: 1,
+                activateOn: 'click',
+                speed: 200
+            });
+
+            window.mobileMidSlider.init();
+
+        }else{
+
+
+            if( window.mobileMidSlider != undefined && window.mobileMidSlider != null ) {
+                window.mobileMidSlider.destroy();
+            }
+
+        }
+    }
 });
