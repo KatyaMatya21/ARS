cf7576a171ae4f8252e3b90fa70e987c9fb808d3
katya <katyamatya21@gmail.com>
2015-07-15 17:41:34 +0400

    catalog-item 666
    1250

--- js/catalog-item.js
+++ js/catalog-item.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
             var targetTab = $(this).data('target'); // tab1
             var tab = $('#'+targetTab); // #tab1
 
-            $(this).parent().parent().find('.active-tab-tab').removeClass('active-tab-tab');
+            $(this).parent().parent().parent().find('.active-tab-tab').removeClass('active-tab-tab');
             tab.addClass('active-tab-tab');
 
             $(this).parent().find('.active-receipt-button').removeClass('active-receipt-button');
@@ -47,5 +47,39 @@ $(document).ready(function(){
         );
 
         slideeSly.init();
+
+        //-----------------------------------------------------------------------------------------
+
+        var lazyResize = _.debounce( onViewportResize, 300 );
+        $(window).on('resize load', lazyResize);
+
+        function onViewportResize() {
+            var win = $(this);
+
+            if( win.width() <= 1450 ) {
+
+                var $slideeFrame = $('.active-super-tab .receipt-content-buttons-frame');
+                var $slideeOptions = {
+                    horizontal: 1,
+                    itemNav: 'basic',
+                    smart: 1,
+                    activateOn: 'click',
+                    speed: 500,
+                    touchDragging: true,
+                    easing: 'swing',
+                    itemSelector: 'li.receipt-button',
+                    prevPage: $('.active-super-tab .control-btn .slider-fader-left').get(0),
+                    nextPage: $('.active-super-tab .control-btn .slider-fader-right').get(0)
+                };
+
+                var slideeSly = new Sly(
+                    $slideeFrame,
+                    $slideeOptions
+                );
+
+                slideeSly.init();
+
+            }
+        }
 
 });
