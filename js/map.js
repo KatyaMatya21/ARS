57feb4aa1f7d7d27c334e51a81d32da6abee95b6
katya <katyamatya21@gmail.com>
2015-07-17 01:11:06 +0400

    map

--- js/map.js
+++ js/map.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+    var gpuAcceleration = Modernizr.csstransforms3d ? 'translateZ(0) ' : '';
+    var transform = Modernizr.prefixed('transform');
+
+
     function initialize() {
         var mapOptions = {
             zoom: 12,
@@ -8,10 +12,47 @@ $(document).ready(function(){
         var map = new google.maps.Map(document.getElementById("map"),
             mapOptions);
 
-        console.log('privet');
-
     }
 
     google.maps.event.addDomListener(window, 'load', initialize);
+
+
+    $('.map-menu-point').bind('click',function(){
+
+        var targetTab = $(this).data('target'); // tab1
+        var tab = $('#'+targetTab); // #tab1
+
+        $('.tab-active-map').removeClass('tab-active-map');
+        tab.addClass('tab-active-map');
+
+        $('.active-map-menu-point').removeClass('active-map-menu-point');
+        $(this).addClass('active-map-menu-point');
+    });
+
+
+    var $slideeFrame = $('.town-list').get(0);
+    var $slideeOptions = {
+        itemNav: 'basic',
+        smart: 1,
+        activateOn: 'click',
+        speed: 500,
+        mouseDragging: 1,
+        touchDragging: 1,
+        easing: 'swing',
+        itemSelector: 'li.town-point',
+        scrollBy: 1
+    };
+
+    var slideeSly = new Sly(
+        $slideeFrame,
+        $slideeOptions
+    );
+
+    slideeSly.init();
+
+    $('.town-point').bind('click',function(){
+        $(this).toggleClass('town-point-switch');
+    });
+
 
 });
