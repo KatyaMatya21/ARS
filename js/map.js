89d92f4e1279807296ee778032d3ca12bf5979fc
katya <katyamatya21@gmail.com>
2015-07-17 00:04:03 +0400

    map js desktop

--- js/map.js
+++ js/map.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
 
+    function initialize() {
+        var mapOptions = {
+            zoom: 12,
+            center: {lat: 59.96705822, lng: 30.3030119}
+        };
+        var map = new google.maps.Map(document.getElementById("map"),
+            mapOptions);
+
+        console.log('privet');
+
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
 });
