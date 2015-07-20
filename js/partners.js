e1bf880d7186caf8e92d734851b7790edeb974b1
katya <katyamatya21@gmail.com>
2015-07-20 15:54:07 +0400

    partners
    work
    features

--- js/partners.js
+++ js/partners.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
 
+    var $slideeFrame = $('.work-slider');
+    var $slideeOptions = {
+        horizontal: 1,
+        itemNav: 'forceCentered',
+        smart: 1,
+        activateOn: 'click',
+        speed: 500,
+        mouseDragging: 1,
+        touchDragging: 1,
+        easing: 'swing',
+        itemSelector: 'li.work-slide',
+        prevPage: $slideeFrame.parent().find('.slider-fader-left').get(0),
+        nextPage: $slideeFrame.parent().find('.slider-fader-right').get(0)
+    };
+
+    var slideeSly = new Sly(
+        $slideeFrame,
+        $slideeOptions
+    );
+
+    slideeSly.init();
+
+
 });
