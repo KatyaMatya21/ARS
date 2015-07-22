5f9460deada9066247dff4dc530fda199bb655cf
katya <katyamatya21@gmail.com>
2015-07-22 17:39:44 +0400

    Энциклопедия Польза
    1250
    
    mobile

--- js/encyclopedia-benefit.js
+++ js/encyclopedia-benefit.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 
+    window.mobileMidSlider = null;
+
+    var gpuAcceleration = Modernizr.csstransforms3d ? 'translateZ(0) ' : '';
+    var transform = Modernizr.prefixed('transform');
+
+    var lazyResize = _.debounce( onViewportResize, 300 );
+    $(window).on('resize load', lazyResize);
+
     $('.tab-question').bind('click',function(){
 
         var targetTab = $(this).data('target'); // tab1
@@ -27,6 +35,36 @@ $(document).ready(function(){
         isOriginLeft: false
     });
 
+    //--------------------------------------------------------------------------------------
 
+    function onViewportResize() {
+        var win = $(this);
+
+        // media breakpoint for 480px
+        if( win.width() <= 480 ) {
+
+
+            // Handle mobile-only mid slider
+            window.mobileMidSlider = new Sly( $('#mobile-mid-slider'), {
+                mouseDragging: 1,
+                touchDragging: 1,
+                horizontal: 1,
+                itemNav: 'forceCentered',
+                smart: 1,
+                activateOn: 'click',
+                speed: 200
+            });
+
+            window.mobileMidSlider.init();
+
+        }else{
+
+
+            if( window.mobileMidSlider != undefined && window.mobileMidSlider != null ) {
+                window.mobileMidSlider.destroy();
+            }
+
+        }
+    }
 
 });
