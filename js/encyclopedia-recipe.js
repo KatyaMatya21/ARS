519d94821226d530e9b2fc2c1d3de45acbf3f072
katya <katyamatya21@gmail.com>
2015-07-28 13:51:01 +0400

    рецепты

--- js/encyclopedia-recipe.js
+++ js/encyclopedia-recipe.js
@@ -1,7 +1,33 @@
 $(function(){
 
     $('.receipt-list > ul > li').click(function(){
-       $(this).toggleClass('active');
+       $(this).addClass('active');
+
+        var $slideePopup = $('.slider-receipt-item').get(0);
+        var $slideeOptions = {
+            itemNav: 'basic',
+            smart: 1,
+            activateOn: 'click',
+            speed: 500,
+            mouseDragging: 1,
+            touchDragging: 1,
+            easing: 'swing',
+            itemSelector: 'li.slide-receipt',
+            scrollBy: 1,
+            scrollSource: $slideePopup,
+            prevPage: $slideePopup.parent().find('.slider-fader-left').get(0),
+            nextPage: $slideePopup.parent().find('.slider-fader-right').get(0)
+        };
+
+        window.slyPopup = new Sly(
+            $slideePopup,
+            $slideeOptions
+        );
+
+        window.slyPopup.init();
+
     });
+
+
 
 });
